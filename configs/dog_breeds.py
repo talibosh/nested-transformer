952a1c4e330c50a790289eea813a655a643d7119
boneh.tali <boneh.tali@gmail.com>
2023-12-18 14:41:24 +0200

    head shape config

--- configs/dog_breeds.py
+++ configs/dog_breeds.py
@@ -37,26 +37,26 @@ def get_config():
     }]
   """
   config = ml_collections.ConfigDict()
-  config.model_name = "nest_base_s196_224"
-  config.per_device_batch_size = 1 #16
+  config.model_name= "nest_base_s196_224"
+  config.per_device_batch_size = 1#16
 
-  config.dataset = "stanford_dogs"
-  config.learning_rate = 2.5e-4
+  config.dataset = "head_shape_stanford_dogs"
+  config.learning_rate = 5e-4 #2.5e-4
   config.optim = "adamw"
   config.optim_wd_ignore = ["pos_embedding"]
   config.grad_clip_max_norm = 0
   config.learning_rate_schedule = "cosine"
   config.warmup_epochs = 0 #20
   config.weight_decay = 0.05
-  config.num_epochs = 1 #300
+  config.num_epochs = 30 #300
   config.num_train_steps = -1
   config.num_eval_steps = -1
 
   config.eval_pad_last_batch = True
-  config.log_loss_every_steps = 5000 #3000
+  config.log_loss_every_steps = 3000 #3000
   config.eval_every_steps = -1
-  config.eval_per_epochs = 1 #10
-  config.checkpoint_every_steps = 5000
+  config.eval_per_epochs = 2 #10
+  config.checkpoint_every_steps = 6000 #5000
   config.shuffle_buffer_size = 1000
 
   config.seed = 42
@@ -64,7 +64,7 @@ def get_config():
 
   # Add randaugment.
   #config.augment = ml_collections.ConfigDict()
-  #config.augment.type = 'default' #"randaugment"  # Set to `default` to disable
+  #config.augment.type = "randaugment"  # Set to `default` to disable
   # All parameters start with `config.augment.randaugment_`.
   #config.augment.randaugment_num_layers = 2
   #config.augment.randaugment_cutout = False
@@ -78,7 +78,7 @@ def get_config():
   # Add mix style augmentation.
   #config.mix = ml_collections.ConfigDict()
   #config.mix.mixup_alpha = 0.8
-  #config.mix.prob_to_apply = 0 #1.0  # Set to 0 to disable
+  #config.mix.prob_to_apply = 1.0  # Set to 0 to disable
   #config.mix.smoothing = 0.1
 
   # Add color jitter.
@@ -88,7 +88,7 @@ def get_config():
   #config.colorjitter.colorjitter_strength = 0.3
   #config.colorjitter.size = 224
 
-  #config.eval_only = True
+  #config.eval_only = False
   config.init_checkpoint = "./checkpoints/nest-b_imagenet/ckpt.39"
   config.reinit_head = "zero_all"
 
