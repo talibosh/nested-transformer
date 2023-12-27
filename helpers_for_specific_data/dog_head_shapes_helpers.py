719f9a0097991588dd5e6766134328ddac512c63
boneh.tali <boneh.tali@gmail.com>
2023-12-27 08:46:25 +0200

    helpers for head shapes. class labels must start from 0 to enable proper model construction

--- helpers_for_specific_data/dog_head_shapes_helpers.py
+++ helpers_for_specific_data/dog_head_shapes_helpers.py
@@ -3,8 +3,16 @@ import numpy as np
 
 allowed_labels_long = tf.constant([9, 18, 20, 25, 26, 61, 62, 63, 80, 81])
 allowed_labels_flat = tf.constant([3, 4, 91, 92, 94, 100, 102, 108])
-allowed_labels = tf.constant([3, 4, 91, 92, 94, 100, 102, 108,9, 18, 20, 25, 26, 61, 62, 63, 80, 81])
+allowed_labels = tf.constant([3, 4, 91, 92, 94, 100, 102, 108,9, 18, 20, 25, 26, 61, 62, 63, 80, 81],  dtype=tf.int32)
+values = tf.constant([1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,0, 0, 0, 0, 0],  dtype=tf.int32)
 
+table = tf.lookup.StaticHashTable(
+    initializer=tf.lookup.KeyValueTensorInitializer(
+        keys=allowed_labels,
+        values=values
+    ),
+    default_value= tf.constant(2,  dtype=tf.int32)
+)
 
 def predicate(x, allowed_labels = allowed_labels):
     label = x['label']
@@ -12,14 +20,21 @@ def predicate(x, allowed_labels = allowed_labels):
     reduced = tf.reduce_sum(tf.cast(isallowed, tf.float32))
     return tf.greater(reduced, tf.constant(0.))
 
+def relabel_map(example):
+    new_label = table.lookup(example['label'])
+    example['label'] = np.full((1,1), new_label, int)
+    return example
 
-def update_label(label, allowed_long=allowed_labels_long, allowed_flat=allowed_labels_flat):
+def update_label(example, allowed_long=allowed_labels_long, allowed_flat=allowed_labels_flat):
     # label = x['label']
-    is_long = tf.equal(allowed_long, tf.cast(label, allowed_long.dtype))
-    is_flat = tf.equal(allowed_flat, tf.cast(label, allowed_flat.dtype))
+    is_long = tf.equal(allowed_long, tf.cast(example['label'], allowed_long.dtype))
+    is_flat = tf.equal(allowed_flat, tf.cast(example['label'], allowed_flat.dtype))
     reduced_long = tf.reduce_sum(tf.cast(is_long, tf.float32))
     reduced_flat = tf.reduce_sum(tf.cast(is_flat, tf.float32))
     if tf.greater(reduced_long, tf.constant(0.)):
-        return np.full((1, 1), 1, dtype=int)
-    else:
-        return np.full((1, 1), 2, dtype=int)
+        example['label']= np.full((1, 1), 0, dtype=int)
+    if tf.greater(reduced_flat, tf.constant(0.)):
+        example['label']= np.full((1, 1), 1, dtype=int)
+    #else:
+    #    example['label']= np.full((1, 1), 2, dtype=int)
+    return example
