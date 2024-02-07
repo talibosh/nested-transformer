87e4560ed82a266f8c8c4e41d903d0978158ac61
boneh.tali <boneh.tali@gmail.com>
2024-02-07 10:57:18 +0200

    cfg update

--- configs/cats_pain.py
+++ configs/cats_pain.py
@@ -38,27 +38,27 @@ def get_config():
   """
   config = ml_collections.ConfigDict()
   config.model_name= "nest_base_s196_224"
-  config.per_device_batch_size = 4#16
+  config.per_device_batch_size = 8 #16
 
   config.dataset = "cats_pain"
-  config.main_dir = "/home/tali/cat_pain1" #location of dataset info
-  config.df_file = "/home/tali/cat_pain1/cats1.csv"
-  config.learning_rate =2.5e-3# 2.5e-4
+  config.main_dir = "/home/tali/cats_pain_proj/face_images/" #location of dataset info
+  config.df_file = "/home/tali/cats_pain_proj/face_images/cats.csv"
+  config.learning_rate =2.5e-4
   config.optim = "adamw"
   config.optim_wd_ignore = ["pos_embedding"]
   config.grad_clip_max_norm = 0
   config.learning_rate_schedule = "cosine"
   config.warmup_epochs = 0 #20
-  config.weight_decay = 0.05
-  config.num_epochs = 30 #300
+  config.weight_decay = 0.005 #0.05
+  config.num_epochs = 60 #300
   config.num_train_steps = -1
   config.num_eval_steps = -1
 
   config.eval_pad_last_batch = True
-  config.log_loss_every_steps = 3000 #3000
+  config.log_loss_every_steps = 500 #3000
   config.eval_every_steps = -1
   config.eval_per_epochs = 10
-  config.checkpoint_every_steps = 6000 #5000
+  config.checkpoint_every_steps = 1000 #5000
   config.shuffle_buffer_size = 1000
 
   config.seed = 42
