cb3be716aa8ec7b0e4e9712a27c77c01e883ca10
boneh.tali <boneh.tali@gmail.com>
2024-02-26 12:17:08 +0200

    minus grads.
    relu removed - scale [0,1]

--- models/nest_for_gradCat.py
+++ models/nest_for_gradCat.py
@@ -121,13 +121,14 @@ class NestForGradCAT():
     def calc_ftr_maps_and_grads_part(self, inputs):
         x, state3, agg_state3 = self.do_bef_grad_level_transformers_3(inputs)
         grad_func3 = jax.grad(self.do_post_grad_level_3)
-        grads3 = grad_func3(x)
+        grads3 = -1*grad_func3(x)
+
         x, state2, agg_state2 = self.do_bef_grad_level_transformers_2(inputs)
         grad_func2 = jax.grad(self.do_post_grad_level_2)
-        grads2 = grad_func2(x)
+        grads2 = -1*grad_func2(x)
         x, state1, agg_state1 = self.do_bef_grad_level_transformers_1(inputs)
         grad_func1 = jax.grad(self.do_post_grad_level_1)
-        grads1 = grad_func1(x)
+        grads1 = -1*grad_func1(x)
         return grads3, grads2, grads1, state3, state2, state1, agg_state3, agg_state2, agg_state1
 
     def calc_ftr_maps_and_grads(self, inputs):
@@ -159,9 +160,9 @@ class NestForGradCAT():
 
         heatmap = conv_output.mean(axis=-1)
 
-        heatmap1 = flax.linen.relu(heatmap) / heatmap.max()
-        #heatmap2 = heatmap / heatmap.max()
-        #heatmap3 = (heatmap - heatmap.min()) / (heatmap.max() - heatmap.min())
+        #heatmap1 = flax.linen.relu(heatmap) / heatmap.max()
+        #heatmap1 = heatmap / heatmap.max()
+        heatmap1 = (heatmap - heatmap.min()) / (heatmap.max() - heatmap.min())
 
         #heatmap3 = heatmap3.reshape(1, heatmap3.shape[0], heatmap3.shape[1], 1)
         #h11_ = flax.linen.avg_pool(heatmap3,
@@ -185,7 +186,4 @@ class NestForGradCAT():
         heatmap2, avg_heatmap2 = self.do_grad_cat_level(state2['intermediates']['features_maps'][0], grads2,
                                             grid_size=(2, 2), patch_size=(14, 14), win_part=4)
         return heatmap3, avg_heatmap3, heatmap2, avg_heatmap2
-
-
-
 
